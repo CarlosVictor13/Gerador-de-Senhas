8b54a336d37426a5e6a9507aa6b94ea6892f0597
Carlos Victor <carlosvictorsantosdasilva14@gmail.com>
2026-07-08 00:42:28 -0300

    Add files via upload
    nova versão 2. 0

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 const btnEl = document.querySelector(".btn");
-
 const inputEl = document.getElementById("input");
+const copyIconEl = document.querySelector(".fa-copy");
+const alertContainerEl = document.querySelector(".alert-container")
 
-const 
+btnEl.addEventListener("click", () => {
+    createPassword();
+});
 
-btnEl.addEventListener("click", ()=>{
-    createPassword()
-})
+copyIconEl.addEventListener("click", () =>{
+    coprPassword();
+    alertContainerEl.classList.remove("active");
+    setTimeout(()=>{
+        alertContainerEl.classList.add("active");
+    }, 2000)
+});
 
 function createPassword() {
     const chars = "1234567890";
     const passwordLength = 8;
-    let password = ""
-    for (let index = 0; index < passwordLength; index++){
-        const randomNum = Math.floor (Math.random() * chars.length);
-password += chars.substring(randomNum, + 1);
+    let password = "";
+
+    for (let index = 0; index < passwordLength; index++) {
+        const randomNum = Math.floor(Math.random() * chars.length);
+        
+        
+        password += chars[randomNum]; 
     }
-    inputEl.value = password;
+
+    inputEl.value = password; 
+    alertContainerEl.innerText = password + "copied"
+}
+
+function copyPassword(){
+    inputEl.ariaSelected();
+    inputEl.setSelectionRange(0, 9999);
+    navigator.clipboard.writeText(inputEl.value);
 }
